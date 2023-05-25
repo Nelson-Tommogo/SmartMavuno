3e24afe3a96fe87b1e2b845bb6d5d75cf155dcac
Tommogo-coder <nelsontommogo9@gmail.com>
2023-05-25 12:28:33 +0300

    Upload

--- upload.js
+++ upload.js
@@ -8,3 +8,19 @@ const firebaseConfig = {
     appId: "1:298910741240:web:dbbe6e3dfcfc1568e9c10d",
     measurementId: "G-N1C5FBGVEE"
   };
+  const form = document.getElementById('imageUploadForm');
+const imageInput = document.getElementById('imageInput');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const file = imageInput.files[0];
+  const storageRef = firebase.storage().ref();
+  const imagesRef = storageRef.child('images/' + file.name);
+  imagesRef.put(file)
+    .then(function(snapshot) {
+      console.log('Image uploaded successfully.');
+    })
+    .catch(function(error) {
+      console.error('Error uploading image:', error);
+    });
+});
